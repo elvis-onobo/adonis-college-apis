aad921f2e61005c376f3b6cb3a5a7df542d1478f
segunisioye <isioyesegun44@hotmail.com>
2020-05-13 05:45:10 +0100

    added admin middleware

---
+++ app/Middleware/Admin.js
@@ -0,0 +1,22 @@
+'use strict'
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+class Admin {
+  /**
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Function} next
+   */
+	async handle({ request, auth }, next) {
+		const user = await auth.getUser()
+
+		if (!user.role_id || user.role_id !== 1)
+			throw new InvalidAccessException()
+		// call next to advance the request
+		await next()
+	}
+}
+
+module.exports = Admin
